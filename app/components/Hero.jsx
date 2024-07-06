5dd2d929ac2c18c259a43ce52ab48f8b14f3f345
skjgit <137107829+skjgit@users.noreply.github.com>
2024-07-06 23:15:21 +0530

    Update Hero.jsx

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -33,7 +33,7 @@ const Hero = ({ login }) => {
                 </div>
                 <div className="text-center">
                     <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-                        75Hard Challenge Tracker  #75HardWithIshan
+                        75Hard Challenge Tracker  #75HardWithYOURSELF
                     </h1>
                     <p className="mt-6 text-lg leading-8 text-gray-600">
                         Embark on a 75-day journey to build transformative habits, unleash potential, and elevate every facet of your life. Start now!
